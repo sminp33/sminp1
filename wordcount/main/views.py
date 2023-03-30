d1561e01a1b4638fda36c205983dd53a67e4d928
sminp33 <tjdals12329@gmail.com>
2023-03-30 13:06:04 +0900

    init

---
+++ wordcount/main/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+
+# Create your views here.
+def main(request):
+    return render(request,'main.html')
+
+def about(request):
+    return render(request,'about.html')
+
+def result(request):
+    text = request.GET['text']
+    text_list = text.split()
+    text_dict = {}
+    for word in text_list :
+        if word in text_dict :
+            text_dict[word] +=1
+        else:
+            text_dict[word] = 1
+    return render(request,'result.html',{'words':text_dict.items()})
